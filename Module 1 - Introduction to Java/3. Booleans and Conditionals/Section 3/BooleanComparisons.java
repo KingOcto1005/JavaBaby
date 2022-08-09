1b92cfede7d945f57a4231ce783c91f4e204a833
KingOcto1005 <opaloctopus1005@gmail.com>
2022-08-09 07:19:40 -0400

    Workbook 3.1

---
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Section 3/BooleanComparisons.java
@@ -0,0 +1,13 @@
+public class BooleanComparisons {
+
+    public static void main(String[] args) {
+
+        int apples = 20;
+        int oranges = 30;
+
+        boolean more = apples > oranges;
+
+        System.out.println( more );
+        
+    }
+}
